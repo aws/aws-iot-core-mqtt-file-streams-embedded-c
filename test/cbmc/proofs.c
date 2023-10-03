dd5f992c23c794dbaed200d691502c313cc8bf4b
bradleysmith23 <bsmith21@bu.edu>
2023-10-03 16:37:25 -0700

    Add cbmc infrastructure

---
+++ test/cbmc/proofs.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "MQTTFileDownloader_base64.h"
+#include "MQTTFileDownloader_cbor.h"
+#include "MQTTFileDownloader.h"
+#include "core_json.h"
+
+
+#ifndef UNWIND_COUNT
+    #define UNWIND_COUNT 10
+#endif
+
+#define CBMC_MAX_OBJECT_SIZE ( PTRDIFF_MAX )
+#define CBMC_MAX_BUFSIZE ( UNWIND_COUNT )
+#define CBMC_STREAMNAME_MAX_LEN (UNWIND_COUNT -1)
+#define CBMC_THINGNAME_MAX_LEN (UNWIND_COUNT -1)
+#define CBMC_TOPIC_MAX_LEN (UNWIND_COUNT -1)
+
+void proof_mqttDownloader_init( void )
+{
+    MqttFileDownloaderContext_t * context = {0};
+    char * streamName;
+    size_t streamNameLength;
+    char * thingName;
+    size_t thingNameLength;
+    uint8_t dataType;
+    uint8_t ret; 
+
+    __CPROVER_assume( streamNameLength <= CBMC_STREAMNAME_MAX_LEN);
+    streamName = malloc(streamNameLength);
+
+    __CPROVER_assume(thingNameLength < CBMC_THINGNAME_MAX_LEN);
+    thingName = malloc(thingNameLength); 
+
+    __CPROVER_assume(dataType <= 255);
+
+    ret = mqttDownloader_init(context,
+                              streamName,
+                              streamNameLength,
+                              thingName,
+                              thingNameLength,
+                              dataType);
+    
+    
+}
+
+void proof_mqttDownloader_createGetDataBlockRequest( void )
+{
+    uint8_t dataType;
+    uint16_t fileId;
+    uint32_t blockSize;
+    uint16_t blockOffset;
+    uint32_t numberOfBlocksRequested;
+    char * getStreamRequest[256] = {0}; 
+    size_t ret;
+
+    __CPROVER_assume(dataType == 0 || dataType == 1);
+
+    __CPROVER_assume(fileId != NULL );
+
+    __CPROVER_assume(blockSize != NULL);
+
+    __CPROVER_assume(blockOffset != NULL);
+
+    __CPROVER_assume(numberOfBlocksRequested != NULL);
+
+    ret = mqttDownloader_createGetDataBlockRequest(dataType,
+                                                   fileId,
+                                                   blockSize,
+                                                   blockOffset,
+                                                   numberOfBlocksRequested,
+                                                   getStreamRequest);
+
+}
+
+int main( )
+{
+    proof_mqttDownloader_init();
+    proof_mqttDownloader_createGetDataBlockRequest();
+}
---
+++ test/cbmc/run_proofs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+coreJSONDir="coreJSON"
+
+UNWIND_COUNT=${UNWIND_COUNT:-10}
+
+#If coreJSON not found, clone it
+if [ ! -d "$coreJSONDir" ]; then
+    git clone https://github.com/FreeRTOS/coreJSON.git
+fi
+
+cd "$(dirname -- "$0")/../../" || exit 1
+
+exec cbmc source/MQTTFileDownloader.c source/MQTTFileDownloader_cbor.c \
+     source/MQTTFileDownloader_base64.c test/cbmc/coreJSON/source/core_json.c \
+     test/cbmc/proofs.c -Isource/include -Itest/cbmc/coreJSON/source/include \
+     -Itest/unit-test/dependencies \
+     --unwindset strlen.0:36 \
+     --bounds-check --pointer-check --memory-cleanup-check --div-by-zero-check \
+     --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check \
+     --conversion-check --undefined-shift-check --enum-range-check \
+     --pointer-primitive-check --drop-unused-functions --nondet-static \
+     --unwinding-assertions --c99 --trace "$@" --unwind "$UNWIND_COUNT" \
+     -DUNWIND_COUNT="$UNWIND_COUNT" >&1 | tee test/cbmc/output/cbmcOutput.txt
