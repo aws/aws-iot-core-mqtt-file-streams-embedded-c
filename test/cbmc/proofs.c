825fb85f16ed6f6ec85cbefa95bf67ce6cb1df9d
bradleysmith23 <bsmith21@bu.edu>
2023-10-05 14:55:19 -0700

    Add cbmc proofs
    Add proofs for base64_decode, CBOR_Decode_GetStreamResponseMessage,
    and CBOR_Encode_GetStreamRequestMessage
    
    Clone tinyCBOR into the cbmc directory if it is not present
    
    Update gitignore to include tinyCBOR
    
    Add stubs for JSON_SearchT and strnlen

--- test/cbmc/proofs.c
+++ test/cbmc/proofs.c
@@ -22,7 +22,7 @@
 
 void proof_mqttDownloader_init( void )
 {
-    MqttFileDownloaderContext_t * context = {0};
+    MqttFileDownloaderContext_t context = {0};
     char * streamName;
     size_t streamNameLength;
     char * thingName;
@@ -38,7 +38,7 @@ void proof_mqttDownloader_init( void )
 
     __CPROVER_assume(dataType <= 255);
 
-    ret = mqttDownloader_init(context,
+    ret = mqttDownloader_init(&context,
                               streamName,
                               streamNameLength,
                               thingName,
@@ -55,12 +55,14 @@ void proof_mqttDownloader_createGetDataBlockRequest( void )
     uint32_t blockSize;
     uint16_t blockOffset;
     uint32_t numberOfBlocksRequested;
-    char * getStreamRequest[256] = {0}; 
+    char * getStreamRequest; 
     size_t ret;
 
-    __CPROVER_assume(dataType == 0 || dataType == 1); 
+    getStreamRequest = malloc(256);
 
-    __CPROVER_assume(fileId != NULL );
+    __CPROVER_assume(dataType == DATA_TYPE_JSON || dataType == DATA_TYPE_CBOR); 
+
+    __CPROVER_assume(fileId != NULL);
 
     __CPROVER_assume(blockSize != NULL);
 
@@ -79,26 +81,16 @@ void proof_mqttDownloader_createGetDataBlockRequest( void )
 
 void proof_mqttDownloader_isDataBlockReceived( void )
 {
-    MqttFileDownloaderContext_t * context = {0};
+    MqttFileDownloaderContext_t  context = {0};
     char * topic;
     size_t topicLength;
     bool ret;
-
-    //__CPROVER_assume(context->dataType != NULL);
-
-    __CPROVER_assume(context->topicGetStream != NULL);
-
-    //__CPROVER_assume(context->topicGetStreamLength != NULL);
-
-    __CPROVER_assume(context->topicStreamData != NULL);
-
-    __CPROVER_assume(context->topicStreamDataLength != NULL);
 
     __CPROVER_assume(topicLength <= CBMC_TOPIC_MAX_LEN);
 
     topic = malloc(topicLength);
 
-    ret = mqttDownloader_isDataBlockReceived(context,
+    ret = mqttDownloader_isDataBlockReceived(&context,
                                              topic,
                                              topicLength);
 
@@ -106,30 +98,20 @@ void proof_mqttDownloader_isDataBlockReceived( void )
 
 void proof_mqttDownloader_processReceivedDataBlock( void )
 {
-    MqttFileDownloaderContext_t * context = {0};
+    MqttFileDownloaderContext_t  context = {0};
     uint8_t * message;
     size_t messageLength;
     uint8_t * data;
     size_t * dataLength;
     bool ret;
 
-    __CPROVER_assume(context->dataType != NULL);
-
-    __CPROVER_assume(context->topicGetStream != NULL);
-
-    __CPROVER_assume(context->topicGetStreamLength != NULL);
-
-    __CPROVER_assume(context->topicStreamData != NULL);
-
-    __CPROVER_assume(context->topicStreamDataLength != NULL);
-
     __CPROVER_assume(messageLength <= CBMC_TOPIC_MAX_LEN);
     message = malloc(messageLength);
 
-    __CPROVER_assume(dataLength <= CBMC_TOPIC_MAX_LEN);
+    __CPROVER_assume(dataLength == 256);
     data = malloc(dataLength);
 
-    ret = mqttDownloader_processReceivedDataBlock(context,
+    ret = mqttDownloader_processReceivedDataBlock(&context,
                                                   message,
                                                   messageLength,
                                                   data,
@@ -137,10 +119,113 @@ void proof_mqttDownloader_processReceivedDataBlock( void )
 
 }
 
+void proof_mqttDownloader_base64_Decode( void )
+{
+    uint8_t * dest;
+    const size_t destLen;
+    size_t * resultLen = 0;
+    const uint8_t * encodedData;
+    const size_t encodedLen;
+    Base64Status_t ret;
+
+    __CPROVER_assume(destLen <= CBMC_MAX_BUFSIZE -1 );
+    dest = malloc(destLen);
+
+    __CPROVER_assume(resultLen != NULL);
+
+    __CPROVER_assume(encodedLen <= CBMC_MAX_BUFSIZE- 1 );
+    encodedData = malloc(encodedLen);
+
+    ret = base64_Decode(dest,
+                        destLen,
+                        resultLen,
+                        encodedData,
+                        encodedLen);
+
+}
+
+void proof_CBOR_Decode_GetStreamResponseMessage( void )
+{
+    const uint8_t * messageBuffer;
+    size_t messageSize;
+    int32_t * fileId;
+    int32_t * blockId;
+    int32_t * blockSize;
+    uint8_t * const * payload;
+    size_t * payloadSize;
+    bool ret; 
+
+    ret = CBOR_Decode_GetStreamResponseMessage(messageBuffer,
+                                               messageSize,
+                                               fileId,
+                                               blockId,
+                                               blockSize,
+                                               payload,
+                                               payloadSize);
+
+
+    
+}
+
+void proof_CBOR_Encode_GetStreamRequestMessage( void )
+{
+    uint8_t * messageBuffer;
+    size_t messageBufferSize;
+    size_t * encodedMessageSize;
+    const char * clientToken;
+    uint32_t fileId;
+    uint32_t blockSize;
+    uint32_t blockOffset;
+    const uint8_t * blockBitmap;
+    size_t blockBitmapSize;
+    uint32_t numOfBlocksRequested;
+    bool ret;
+
+    __CPROVER_assume(messageBuffer != NULL);
+
+    __CPROVER_assume(messageBufferSize != NULL);
+
+    __CPROVER_assume(encodedMessageSize != NULL);
+
+    __CPROVER_assume(clientToken != NULL);
+
+    __CPROVER_assume(fileId != NULL);
+
+    __CPROVER_assume(blockSize != NULL);
+
+    __CPROVER_assume(blockOffset != NULL);
+
+    __CPROVER_assume(blockBitmap != NULL);
+
+    __CPROVER_assume(blockBitmapSize != NULL);
+
+    __CPROVER_assume(numOfBlocksRequested != NULL);
+
+
+
+    ret = CBOR_Encode_GetStreamRequestMessage(messageBuffer,
+                                              messageBufferSize,
+                                              encodedMessageSize,
+                                              clientToken,
+                                              fileId,
+                                              blockSize,
+                                              blockOffset,
+                                              blockBitmap,
+                                              blockBitmapSize,
+                                              numOfBlocksRequested);
+}
+
 int main( )
 {
+    /* Functions in MQTTFileDownloader.c */
     proof_mqttDownloader_init();
     proof_mqttDownloader_createGetDataBlockRequest();
     proof_mqttDownloader_isDataBlockReceived();
     proof_mqttDownloader_processReceivedDataBlock();
+    /* Functions in MQTTDownloader_base64.c */
+    proof_mqttDownloader_base64_Decode();
+    /* Functions in MQTTDownloader_cbor.c */
+    // proof_CBOR_Decode_GetStreamResponseMessage();
+    // proof_CBOR_Encode_GetStreamRequestMessage();
+
 }
--- test/cbmc/run_proofs.sh
+++ test/cbmc/run_proofs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 coreJSONDir="coreJSON"
+tinyCborDir="tinyCBOR"
 
 UNWIND_COUNT=${UNWIND_COUNT:-10}
 
@@ -9,16 +10,25 @@ if [ ! -d "$coreJSONDir" ]; then
     git clone https://github.com/FreeRTOS/coreJSON.git --depth 1 --branch v3.2.0
 fi
 
-cd "$(dirname -- "$0")/../../" || exit 1
+#If tinyCBOR not found, clone it
+if [ ! -d "$tinyCborDir" ]; then
+    git clone https://github.com/intel/tinycbor.git --depth 1 --branch v0.6.0
+fi
 
-exec cbmc source/MQTTFileDownloader.c source/MQTTFileDownloader_cbor.c \
-     source/MQTTFileDownloader_base64.c test/cbmc/coreJSON/source/core_json.c \
-     test/cbmc/proofs.c -Isource/include -Itest/cbmc/coreJSON/source/include \
-     -Itest/unit-test/dependencies \
+MQTTStreamingSourceDir="../../source"
+
+exec cbmc proofs.c $MQTTStreamingSourceDir/MQTTFileDownloader.c \
+     $MQTTStreamingSourceDir/MQTTFileDownloader_cbor.c \
+     $MQTTStreamingSourceDir/MQTTFileDownloader_base64.c proofs.c  \
+     $tinyCborDir/src/cborencoder.c $tinyCborDir/src/cborencoder_close_container_checked.c \
+     stubs/strnlen.c stubs/JSON_SearchT.c proofs.c \
+     -I $MQTTStreamingSourceDir/include -I coreJSON/source/include  -I tinyCBOR/src \
      --unwindset strlen.0:36 \
+     --unwindset __builtin___strncat_chk.0:192 \
+     --unwindset __builtin___strncat_chk.1:205 \
      --bounds-check --pointer-check --memory-cleanup-check --div-by-zero-check \
      --signed-overflow-check --unsigned-overflow-check --pointer-overflow-check \
      --conversion-check --undefined-shift-check --enum-range-check \
      --pointer-primitive-check --drop-unused-functions --nondet-static \
      --unwinding-assertions --c99 --trace "$@" --unwind "$UNWIND_COUNT" \
-     -DUNWIND_COUNT="$UNWIND_COUNT" >&1 | tee test/cbmc/output/cbmcOutput.txt
+     -DUNWIND_COUNT="$UNWIND_COUNT" >&1 | tee output/cbmcOutput.txt
