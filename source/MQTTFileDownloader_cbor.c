1d6a7581aeef2f794d0a2688da60f381c13ca965
bradleysmith23 <bsmith21@bu.edu>
2023-11-17 11:42:23 -0800

    Update copyright info

--- source/MQTTFileDownloader_cbor.c
+++ source/MQTTFileDownloader_cbor.c
@@ -1,6 +1,6 @@
 /*
  * AWS IoT Core MQTT File Streams Embedded C v1.0.0
- * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (C) 2023 Amazon.com, Inc. and its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: MIT
  *
  * Licensed under the MIT License. See the LICENSE accompanying this file
