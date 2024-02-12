8312466036de223a6a728443491c4d9ba1473282
AniruddhaKanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2024-02-12 15:16:53 -0800

    Move base64 decode table out of function

--- source/include/MQTTFileDownloader.h
+++ source/include/MQTTFileDownloader.h
@@ -60,7 +60,7 @@
  * @ingroup mqtt_file_downloader_const_types
  * @brief Configure the Maximum size of the data payload.
  */
-#define mqttFileDownloader_CONFIG_BLOCK_SIZE    256U
+#define mqttFileDownloader_CONFIG_BLOCK_SIZE    512U
 
 /**
  * @brief Macro to calculate string length.
