224b58078539a31f67c9313b6fa317efdbf4bdf5
Rahul Kar <karahulx@amazon.com>
2024-05-27 19:33:47 +0000

    Update version number and add changelog

--- source/MQTTFileDownloader_base64.c
+++ source/MQTTFileDownloader_base64.c
@@ -1,5 +1,5 @@
 /*
- * AWS IoT Core MQTT File Streams Embedded C v1.0.0
+ * AWS IoT Core MQTT File Streams Embedded C v1.1.0
  * Copyright (C) 2023 Amazon.com, Inc. and its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: MIT
  *
